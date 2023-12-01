4f7cd1ab77d14b3a97956798f0de6bbe38e4c9b3
Fahim Iftekhar Efto <ifto.141.fi@gmail.com>
2023-12-01 14:42:34 +0600

    enum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -18,11 +19,19 @@ async def rootPost():
 async def rootDelete():
     return {"message": "Hello World DELETE"}
 
+class userEnum(str, Enum):
+    efto = "efto"
+    efto2 = "efto2"
+    efto3 = "efto3"
+    
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: userEnum):
+    if(item_id == userEnum.efto):
+        return {"item_id": item_id}
+    return {"item_id": "not efto"}
 
 @app.get("/items")
 async def get_items():
     return {"items": "items"}
+
