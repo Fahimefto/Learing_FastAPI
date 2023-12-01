63ee0c751a775ce63ba468959736b4fcbb5dde98
Fahim Iftekhar Efto <ifto.141.fi@gmail.com>
2023-12-01 14:52:02 +0600

    query params

--- main.py
+++ main.py
@@ -24,14 +24,23 @@ class userEnum(str, Enum):
     efto2 = "efto2"
     efto3 = "efto3"
     
-
+# enum example
 @app.get("/items/{item_id}")
 async def read_item(item_id: userEnum):
     if(item_id == userEnum.efto):
         return {"item_id": item_id}
     return {"item_id": "not efto"}
 
+# query example
+local_data = [
+    {"id": 1, "name": "efto"},
+    {"id": 2, "name": "efto2"},
+    {"id": 3, "name": "efto3"}
+]
+
+
 @app.get("/items")
-async def get_items():
-    return {"items": "items"}
+async def get_items(skip: int = 0, limit: int = 10):
+    return local_data[skip : skip + limit]
+    
 
